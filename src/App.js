2770acf4560c813934009a97a42935e94d8580f3
shch989 <tankpop11@gmail.com>
2022-10-20 19:16:46 +0900

    jsx 뜯어보기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -29,10 +30,17 @@ function App() {
     },
   ];
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, {items: expenses})
+  // )
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses items={expenses}></Expenses>
+      <Expenses items={expenses}/>
     </div>
   );
 }
