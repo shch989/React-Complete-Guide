0055e1348a876a3419e7269ea614316c079ef99d
shch989 <tankpop11@gmail.com>
2022-10-22 20:19:40 +0900

    버튼기능 추가 및 코드 정리

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 import './Expenses.css'
 
@@ -23,14 +23,7 @@ const Expenses = (props) => {
          selected={filteredYear} 
          onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem 
-            key={expense.id}
-            title={expense.title} 
-            amount={expense.amount} 
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div> 
   )
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enterExpenseData) => {
     const expenseData = {
       id: Math.random().toString(),
@@ -13,9 +15,18 @@ const NewExpense = (props) => {
     props.onAddExpense(expenseData)
   }
 
+  const startEditingHandler = () => {
+    setIsEditing(true)
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false)
+  }
+
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData ={saveExpenseDataHandler}/>
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      { isEditing && <ExpenseForm onSaveExpenseData ={saveExpenseDataHandler} onCancel={stopEditingHandler}/> }
     </div>
   )
 }
